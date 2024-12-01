a06627f583eac80bafdd08bc4d42149dd504e7de
InFynnity8 <sackeyemmanuelfynn@gmail.com>
2024-12-01 21:40:20 +0000

    levelorder

--- Data_structures/binary_tree.py
+++ Data_structures/binary_tree.py
@@ -1,4 +1,25 @@
 # pylint: disable=missing-class-docstring
+class Queue:
+    def __init__(self):
+        self.queue = []
+        
+    def enqueue(self, item):
+        self.queue.append(item)
+    
+    def dequeue(self):
+        if self.is_empty():
+            return print('Queue is empty')
+        return self.queue.pop(0)
+    
+    def peek(self):
+        if self.is_empty():
+            return print('Queue is empty')
+        return self.queue[0].value
+    
+    def size(self):
+        return len(self.queue) 
+    def is_empty(self):
+        return len(self.queue) == 0
 class TreeNode:
     def __init__(self, value):
         self.value = value
@@ -17,6 +38,8 @@ class Binary_tree:
             return self.preorder_traverse(self.root, "")
         if trav_type == "postorder":
             return self.postorder_traverse(self.root, "")
+        if trav_type == "levelorder":
+            return self.levelorder_traverse(self.root)
         return print(
             "Invalid traversal type. Choose from inorder, preorder, or postorder."
         )
@@ -41,6 +64,20 @@ class Binary_tree:
             traversal = self.postorder_traverse(start.right, traversal)
             traversal += str(start.value) + " -> "
         return traversal
+    def levelorder_traverse(self, start):
+        if not start:
+            return
+        queue = Queue()
+        queue.enqueue(start)
+        traversal = ""
+        while queue.size() > 0:
+            node = queue.dequeue()
+            traversal += str(node.value) + " -> "
+            if node.left:
+                queue.enqueue(node.left)
+            if node.right:
+                queue.enqueue(node.right)
+        return traversal
 
 
 tree = Binary_tree(1)
@@ -59,3 +96,5 @@ print('InOrder')
 print(tree.print_traverse("inorder"))
 print('PostOrder')
 print(tree.print_traverse("postorder"))
+print('LevelOrder')
+print(tree.print_traverse("levelorder"))
